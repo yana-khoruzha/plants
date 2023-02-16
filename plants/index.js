875e85c666f6989fd433fe1b233fe5817d4b2675
YandaPanda19 <i.am.kitiket@gmail.com>
2023-02-16 02:13:34 +0200

    change js

--- plants/index.js
+++ plants/index.js
@@ -26,62 +26,10 @@ document.addEventListener("click", function (event) {
   }
 });
 
-
-const buttonsDOM = document.querySelectorAll(".servBtn");
-const cardsDOM = document.querySelectorAll(".blocks");
-
-const btnsState = [false, false, false];
-const cardsBlured = [false, false, false, false, false, false];
-
-function btnClick(btnIndex) {
-  btnsState[btnIndex] = true;
-  console.log(btnsState[btnIndex]);
-  // for (let i = 0; i < buttonsDOM.length; i++) {
-  //   buttonsDOM[i].addEventListener("click", function () {
-  //     btnsState[i] = true;
-  //     console.log(btnsState[i]);
-  //   });
-  // }
-
-  if (btnsState[0] == true) {
-    cardsBlured[1] = true;
-    cardsBlured[2] = true;
-    cardsBlured[3] = true;
-    cardsBlured[5] = true;
-  }
-  if (btnsState[1] == true) {
-    cardsBlured[0] = true;
-    cardsBlured[1] = true;
-    cardsBlured[3] = true;
-    cardsBlured[4] = true;
-    cardsBlured[5] = true;
-  }
-  if (btnsState[2] == true) {
-    cardsBlured[0] = true;
-    cardsBlured[2] = true;
-    cardsBlured[4] = true;
-  }
-
-  for (let i = 0; i < btnsState.length; i++) {
-    if (btnsState[i] == true) {
-      buttonsDOM[i].classList.add("button:active");
-    }
-  }
-
-  for (let i = 0; i < cardsBlured.length; i++) {
-    if (cardsBlured[i] == true) {
-      cardsDOM[i].classList.add("blur");
-    }
-  }
-}
-
-let counter = 0;
-
-buttonsDOM.forEach((button, index) =>
-  button.addEventListener("click", () => btnClick(index))
-);
-
-alert(
-  "Привет! Пожалуйста, не проверяйте пока мою работу, можно мне еще несколько дней? Ничего не успеваю, но закончить хочется... Буду очень благодарна."
-);
-
+let plantings = document.querySelectorAll(".planting");
+let gardens = document.querySelectorAll(".gardens");
+let lawns = document.querySelectorAll(".lawn");
+let gardensBtn = document.querySelector(".gardensBtn");
+let lawnBtn = document.querySelector(".lawnBtn");
+let plantingBtn = document.querySelector(".plantingBtn");
+let cardsDOM = document.querySelectorAll(".blocks");
